007e6caaa888acd19cebad1b3411ba31b625a14a
myasinK <yasinul@gmail.com>
2018-09-19 22:56:57 -0700

    Match string with regex

---
+++ Kata-RegexPIN-Validation/Kata.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Kata_RegexPIN_Validation
+{
+    public static class Kata
+    {
+        public static bool RegexMatch(int pin)
+        {
+            var pattern = @"^\d{4}$|^\d{6}$";
+            var regex = new Regex(pattern);
+            return regex.IsMatch(pin.ToString());
+        }
+    }
+}
---
+++ Kata-RegexPIN-Validation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata_RegexPIN_Validation
+{
+    //ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+
+    //If the function is passed a valid PIN string, return true, else return false.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kata.RegexMatch(123);
+        }
+    }
+}
